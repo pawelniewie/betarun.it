2cbc067d231f0b708bc226303bc6e0b6a6c11816
Pawel Niewiadomski <11110000b@gmail.com>
2013-09-01 10:47:22 +0200

    first version that can store appcasts in mongo

--- Gemfile
+++ Gemfile
@@ -1,7 +1,14 @@
 source "https://rubygems.org"
 ruby '2.0.0'
 
+gem 'mongo'
+gem 'mongoid'
+gem 'bson_ext'
 gem 'sinatra'
 gem "haml"
 gem "sass"
 gem "compass"
+
+group :development do
+	gem 'sinatra-reloader'
+end
--- web.rb
+++ web.rb
@@ -2,6 +2,8 @@ require "bundler"
 Bundler.setup(:default)
 Bundler.require
 
+Mongoid.load!("mongoid.yml")
+
 configure do
   set :public_dir, File.dirname(__FILE__) + '/public'
   set :static, true
@@ -12,6 +14,17 @@ configure do
   Compass.add_project_configuration(File.join(Sinatra::Application.root, 'config', 'compass.rb'))
 end
 
+class AppCast
+    include Mongoid::Document
+
+    field :name, type: String
+
+    index({ name: 1 }, { unique: true })
+end
+
+helpers do
+
+end
 
 get '/' do
 	haml :index
@@ -19,4 +32,26 @@ end
 
 get '/queued/?' do
 	haml :queued
+end
+
+get '/appcasts.json' do
+	content_type :json
+	appcasts = AppCast.all
+	appcasts.to_json
+end
+
+post '/appcasts.json' do
+	content_type :json
+	AppCast.create!(name: params[:name]).to_json
+end
+
+put '/appcasts.json/:id' do |id|
+	content_type :json
+	data = JSON.parse(request.body.read)
+	AppCast.find(id).update(data).to_json
+end
+
+get '/appcasts.json/:id' do |id|
+	content_type :json
+	AppCast.find(id).to_json
 end
