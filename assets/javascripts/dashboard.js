ab2264b0e1f4fa4d77f088b7a94ac8f82dd2188a
Pawel Niewiadomski <11110000b@gmail.com>
2013-09-14 21:59:52 +0200

    added ordering to versions, updating drag and drop

--- assets/javascripts/dashboard.js
+++ assets/javascripts/dashboard.js
@@ -26,16 +26,7 @@ factory('Appcasts', ['$http', function($http) {
 		.when('/', {controller: VersionsCtrl, templateUrl: '/partials/versions'})
 		.when('/edit/:versionId', {controller: EditVersionCtrl, templateUrl: '/partials/edit-version'})
 		.otherwise({ redirectTo: '/'});
-}])
-.filter('bytes', function() {
-	return function(bytes, precision) {
-		if (isNaN(parseFloat(bytes)) || !isFinite(bytes)) return '-';
-		if (typeof precision === 'undefined') precision = 1;
-		var units = ['bytes', 'kB', 'MB', 'GB', 'TB', 'PB'],
-			number = Math.floor(Math.log(bytes) / Math.log(1024));
-		return (bytes / Math.pow(1024, Math.floor(number))).toFixed(precision) +  ' ' + units[number];
-	};
-});
+}]);
 
 var VersionsCtrl = ['$scope', '$log', '$http', '$location', 'Appcasts', function VersionsCtrl($scope, $log, $http, $location, Appcasts) {
 	$scope.$log = $log;
